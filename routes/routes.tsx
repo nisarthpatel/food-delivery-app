a742673fd8f707593ab9aafe37391315c1b09c1d
nisarthpatel <patelnisarth@gmail.com>
2024-12-19 18:32:36 +0530

    added routing and bottom tabs behaviour

--- routes/routes.tsx
+++ routes/routes.tsx
@@ -1,16 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import OnBoardingScreen from '../screens/OnBoardingScreen';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import CartScreen from '../screens/CartScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import HomeScreen from '../screens/HomeScreen';
+import NotificationsScreen from '../screens/NotificationsScreen';
+import OnBoardingScreen from '../screens/OnBoardingScreen';
+
+const Tab = createBottomTabNavigator<RootStackParamList>();
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export type RootStackParamList = {
     OnBoardingScreen: undefined;
     HomeScreen: undefined;
+    FavoritesScreen: undefined;
+    CartScreen: undefined;
+    NotificationsScreen: undefined;
 };
+
+const HomeTabs = () => (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="HomeScreen" component={HomeScreen} />
+        <Tab.Screen name="CartScreen" component={CartScreen} />
+        <Tab.Screen name="FavoritesScreen" component={FavoritesScreen} />
+        <Tab.Screen name="NotificationsScreen" component={NotificationsScreen} />
+    </Tab.Navigator>
+);
 
 export const MyStack = () => {
     return (
@@ -19,12 +38,10 @@ export const MyStack = () => {
                 initialRouteName="OnBoardingScreen"
                 screenOptions={{ headerShown: false }}>
                 <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
-                <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="HomeScreen" component={HomeTabs} />
             </Stack.Navigator>
         </NavigationContainer>
-    )
-}
+    );
+};
 
-
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
